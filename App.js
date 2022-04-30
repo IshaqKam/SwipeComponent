f19a303d2084a5edcce3331b15c83f5cda7611cb
IshaqKam <ishkam2001@gmail.com>
2022-05-01 02:36:07 +0500

    'Final Commit'

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
-import SwipeComponent from './SwipeComponent';
+import SwipeComponent from './components/SwipeComponent';
 
 export default function App() {
 
